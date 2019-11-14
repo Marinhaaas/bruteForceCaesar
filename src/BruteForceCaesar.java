12a0c151b37eb51509dda4de67ccd44774a5618d
Marinheiro <rubenepmarinheiro@gmail.com>
2019-11-14 05:17:05 +0000

    Version 2 - Little changes to increase performance

--- src/BruteForceCaesar.java
+++ src/BruteForceCaesar.java
@@ -8,26 +8,27 @@ import java.util.Scanner;
 
 public class BruteForceCaesar {
     public static void main(String[] args) {
+        System.out.println("Running in background exploring all the of 4271389824 combinations...");
         long start = System.currentTimeMillis();
         String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String salt, actualPre, actualSuf;
+        String actualPre, actualSuf;
+        StringBuilder saltBuilder = new StringBuilder();
         int totalCount = 0;
         ArrayList<String> words = readWordList(args[1]);
+        String[] newWords = words.toArray(new String[words.size()]);
 
         for (int i = 0; i <= 51; i++) {
             for (int j = 0; j <= 51; j++) {
-                for (int k = 0; k < words.size(); k++) {
+                for (int k = 0; k < newWords.length; k++) {
                     for (int l = 0; l <= 51; l++) {
                         totalCount += 1;
-                        salt = new StringBuilder().append(alphabet.charAt(i)).append(alphabet.charAt(j)).toString();
-                        actualPre = textToHash(salt + caesar(words.get(k), l));
-                        actualSuf = textToHash(caesar(words.get(k), l) + salt);
-                        if (!actualPre.equals(args[0]) && !actualSuf.equals(args[0])) {
-                            System.out.println(totalCount + " of 4271389824");
-                        } else {
+                        saltBuilder.append(alphabet.charAt(i)).append(alphabet.charAt(j));
+                        actualPre = textToHash(saltBuilder.toString() + caesar(newWords[k], l, alphabet));
+                        actualSuf = textToHash(caesar(newWords[k], l, alphabet) + saltBuilder.toString());
+                        if (actualPre.equals(args[0]) || actualSuf.equals(args[0])) {
                             System.out.println();
                             System.out.println("--------------------------------");
-                            System.out.println("Stop! The secret word is: " + words.get(k));
+                            System.out.println("Stop! The secret word is: " + newWords[k]);
                             System.out.println("--------------------------------");
                             System.out.println();
 
@@ -41,10 +42,12 @@ public class BruteForceCaesar {
                             } else {
                                 System.out.println("Salt Position: Suffix");
                             }
-                            System.out.println("Salt: " + salt);
+                            System.out.println("Salt: " + saltBuilder.toString());
                             System.out.println("Caesar shift: " + l);
 
                             System.exit(1);
+                        } else {
+                            saltBuilder.setLength(0);
                         }
                     }
                 }
@@ -52,7 +55,7 @@ public class BruteForceCaesar {
         }
     }
 
-    public static String textToHash(String plainText) {
+    private static String textToHash(String plainText) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             byte[] messageDigest = md.digest(plainText.getBytes());
@@ -67,25 +70,25 @@ public class BruteForceCaesar {
         }
     }
 
-    public static String caesar(String msg, int shift) {
-        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String caesar(String msg, int shift, String alphabet) {
+        StringBuilder nomes = new StringBuilder();
         int actualIndex, count;
         String character;
-        String result = "";
+        String result = null;
         for (int i = 0; i < msg.length(); i++) {
             character = String.valueOf(msg.charAt(i));
             actualIndex = alphabet.indexOf(character);
             if ((actualIndex + shift) >= alphabet.length()) {
                 count = (actualIndex + shift) / 52;
-                result += String.valueOf(alphabet.charAt((actualIndex + shift) - (52 * count)));
+                result = nomes.append(alphabet.charAt((actualIndex + shift) - (52 * count))).toString();
             } else {
-                result += String.valueOf(alphabet.charAt(actualIndex + shift));
+                result = nomes.append(alphabet.charAt(actualIndex + shift)).toString();
             }
         }
         return result;
     }
 
-    public static ArrayList<String> readWordList(String path) {
+    private static ArrayList<String> readWordList(String path) {
         ArrayList<String> words = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(new File(path));
